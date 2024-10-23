02d495ef803fe812d0242c376af5e36f7b5e0d25
proflobo <matheus.v.lobo@gmail.com>
2024-10-23 11:01:37 -0300

    Initial commit

---
+++ Assets/Scripts/bala.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bala : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Destroy(this.gameObject, 5);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
